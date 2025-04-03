0fcc5493d506e4dcadec88242191d3a28da9c6e7
TheAsad0 <107338968+TheAsad0@users.noreply.github.com>
2025-04-03 15:11:37 -0600

    Reverting backend to asad-branch

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,29 +2,19 @@ import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
 
 const authenticateJWT = (req, res, next) => {
-    
-    console.log(req.headers['authorization'],'auth')
-    
-    //not work?
-    //const token = req.header('Authorization')?.replace('Bearer ', '');  
-    const token = req.headers['authorization']
-
+    const token = req.header('Authorization')?.replace('Bearer ', '');
     if(!token) {
         return res.status(401).json({ message: 'Authentication required' });
     }
-    
-    //process.env.JWT_SECRET ->secret, cause issue
-    jwt.verify(token, "secret", async (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
        if(err) {
         return res.status(403).json({ message: 'Invalid token - Please Login Again' });
        }
-
        try {
         const user = await User.findByPk(decoded.id);
         if(!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-
         req.user = user; // Attach user to the request object
         next();
        } catch (error) {
