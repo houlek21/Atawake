97ce31f151d37af0e0b76604010b9b9dc2eb0f2a
TheAsad0 <107338968+TheAsad0@users.noreply.github.com>
2025-04-03 15:12:31 -0600

    Reverting backend to asad-branch

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,19 +6,18 @@ import authenticateJWT from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 // public routes
-router.post('/login', loginUser)
+router.post('/login', loginUser);
 router.post('/', addUser);
-router.post('/find', getUser);
-//router.get('/:id', getUser);
 router.get('/', getAllUsers);
 router.get('/validate-token', authenticateJWT, validateToken);
 
+router.get('/:id', getUser);
 router.delete('/:id', deleteUser);
 
 // CRUD routes for users (protected by JWT)
-//router.get('/', authenticateJWT, getAllUsers); // Get all users (TODO: implement admin only logic)
+// router.get('/', authenticateJWT, getAllUsers); // Get all users (TODO: implement admin only logic)
 router.put('/:id', authenticateJWT, updateUser); // Update user
-//router.delete('/:id', authenticateJWT, deleteUser); // Delete user (TODO: implement admin only logic)
+// router.delete('/:id', authenticateJWT, deleteUser); // Delete user (TODO: implement admin only logic)
 
 
 export default router;
